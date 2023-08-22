724f6fe1dbf6655cb6f9c1b8b7ee16f8222eaf31
Sndsh10 <115711081+Sndsh10@users.noreply.github.com>
2023-08-22 16:27:56 +0100

    Update DistanceUnits.cs

--- ConsoleAppProject/App01/DistanceUnits.cs
+++ ConsoleAppProject/App01/DistanceUnits.cs
@@ -1,17 +1,1 @@
-﻿using System.ComponentModel.DataAnnotations;
 
-namespace ConsoleAppProject.App01
-{
-    /// <summary>
-    /// List of units used to measure distance
-    /// </summary>
-    public enum DistanceUnits
-    {
-        [Display(Name = "No Unit")]
-        NoUnit,
-        Feet,
-        Metres,
-        Kilometres,
-        Miles
-    }
-}
